cf33339927f643da945f747ce6abb9f950b9fc46
Thiago-Batista314 <126125782+Thiago-Batista314@users.noreply.github.com>
2023-09-27 17:13:08 -0300

    Add files via upload

--- Probability_calculator/prob_calculator.py
+++ Probability_calculator/prob_calculator.py
@@ -2,41 +2,45 @@ import copy
 import random
 # Consider using the modules imported above.
 
+# The commentaries at side of codes are exemplifing what its piece of code does;
 class Hat:
     def __init__(self, **kwargs):
-        self.contents = [k for k, v in kwargs.items() for _ in range(v)]
+        # Builds the list with the color n-times;
+        self.contents = [k for k, v in kwargs.items() for _ in range(v)] # Ex: b = ['blue', 'blue', 'red', 'red', 'green', 'green', 'green']
     
     def draw(self, drawn):
+        # Switches case drawn > len(self.contents), or does nothing otherwise;
         n = min(drawn, len(self.contents))
         drawn_balls = []
 
+        # Add randomly the balls to the drawn_balls list and
+        # remove from self.contents list;
         for _ in range(n):
             pos = random.randint(0, len(self.contents)-1)
-            drawn_balls.append(self.contents[pos])
-            self.contents.pop(pos)
+            drawn_balls.append(self.contents[pos]) # Ex: ['blue', 'red']
+            self.contents.pop(pos) # b ~> ['blue', 'red', 'red', 'green', 'green']
 
         return drawn_balls
 
-    
+
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
     m = 0
     for _ in range(num_experiments):
+        # Do a copy of hat class;
         experiment_hat = copy.deepcopy(hat)
-        drawn_balls = experiment_hat.draw(num_balls_drawn)
-        balls_req = []
-        for k, v in expected_balls.items():
-            if drawn_balls.count(k) >= v:
-                balls_req.append(1)
-        balls_req = sum(balls_req)
-        if balls_req == len(expected_balls):
-            m += 1
-    probability = m / num_experiments
-    return probability
 
-if __name__ == '__main__':
-    hat = Hat(black=6, red=4, green=3)
-    probability = experiment(hat=hat,
-                  expected_balls={"red": 2, "green": 1},
-                  num_balls_drawn=5,
-                  num_experiments=2000)
-    print(probability)
+        # Build a list with drawn balls of the copied hat;
+        drawn_balls = experiment_hat.draw(num_balls_drawn)
+
+        balls_req = []
+
+        # Calculates the probability of drawn_balls;
+        for k, v in expected_balls.items(): # Ex: expected_balls = {'green': 2}
+            if drawn_balls.count(k) >= v:
+                balls_req.append(1) # If green went to the drawn_balls list 2 or more times;
+        balls_req = sum(balls_req) # Sum of times green went to that list;
+        if balls_req == len(expected_balls): # Add 1 to m variable if the times of ball went == len(expected_balls);
+            m += 1                           # So, following the current example in coments, we'll have two possible results:
+    probability = m / num_experiments        # 1º: green is in drawn_balls < 1 times. ~> m won't add 1 because len(expected) = 1;
+                                             # 2º: green is in drawn_balls >= 1 times. ~> m add 1 because len(expected) = 1;
+    return probability                       
